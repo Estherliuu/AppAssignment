2270eb8cb16f69cd8ec46e04d5b2a9fc1c1d5837
Esther L <Estherliu@Esthers-MacBook-Air.local>
2018-10-09 18:47:41 +1100

    updated gitignore

--- app/src/main/java/com/example/estherliu/appassignment/MainActivity.java
+++ app/src/main/java/com/example/estherliu/appassignment/MainActivity.java
@@ -12,14 +12,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
+
+
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -98,4 +105,6 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
 }
