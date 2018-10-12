cd2c1b837d0403583846b9192c15b0985c3c4e6c
Esther L <Estherliu@Esthers-MacBook-Air.local>
2018-10-12 21:45:19 +1100

    Splashscreen made!

--- app/src/main/java/com/example/estherliu/appassignment/LandingPage.java
+++ app/src/main/java/com/example/estherliu/appassignment/LandingPage.java
@@ -1,13 +1,19 @@
 package com.example.estherliu.appassignment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 public class LandingPage extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landing_page);
+
     }
 }
